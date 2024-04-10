495ffaa83d6a6cb40567b714f7873923ba73953f
Ravitejamangilipelly <ravitejamangilipelly586@gamail.com>
2024-04-10 05:26:06 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.get('/gadgets', (request, response) => {
+  response.sendFile('./gadgets.html', {root: __dirname})
+})
+
+module.exports = app
